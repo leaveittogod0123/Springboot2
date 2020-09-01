f795faa175e2fce581b20bef819db1f73c78a69e
yosephNoh <yoseph.noh@solarconnect.kr>
2020-09-02 02:28:47 +0900

    수정 테스트까지

--- src/main/java/com/yobs/playground/springboot2/service/PostsService.java
+++ src/main/java/com/yobs/playground/springboot2/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.yobs.playground.springboot2.service;
 
+import com.yobs.playground.springboot2.domain.posts.Posts;
 import com.yobs.playground.springboot2.domain.posts.PostsRepository;
+import com.yobs.playground.springboot2.web.dto.PostsResponseDto;
 import com.yobs.playground.springboot2.web.dto.PostsSaveRequestDto;
+import com.yobs.playground.springboot2.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
@@ -14,5 +17,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById ( Long id ) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id"+id));
+
+        return new PostsResponseDto(entity);
     }
 }
